ecda9d3c6b67d01a6b66275a80531d6fab6d3c02
AhnHyungJoon <104292970+AhnHyungJoon@users.noreply.github.com>
2022-05-30 02:10:06 +0900

    [Bronze V] Title: 검증수, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -3,18 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
-        int e = sc.nextInt();
-        int result1 = (int) Math.pow(a, 2);
-        int result2 = (int) Math.pow(b, 2);
-        int result3 = (int) Math.pow(c, 2);
-        int result4 = (int) Math.pow(d, 2);
-        int result5 = (int) Math.pow(e, 2);
-        int sum = result1 + result2 + result3 + result4 + result5;
-        int result = sum % 10;
+        int[] num = new int[5];
+        for(int i=0; i<5; i++){
+            num[i] = sc.nextInt();
+        }
+        int result = 0;
+        for(int i=0; i<5; i++) {
+          result += num[i]*num[i];
+        }
+        result %= 10;
         System.out.println(result);
     }
 }
